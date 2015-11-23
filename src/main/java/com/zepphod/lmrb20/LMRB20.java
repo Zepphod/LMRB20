a98e28c228db786379dd272afd67d13ca8ac5731
Zepphod <zepphod@gmail.com>
2015-11-23 04:17:26 +0100

    1

---
+++ src/main/java/com/zepphod/lmrb20/LMRB20.java
@@ -0,0 +1,35 @@
+package com.zepphod.lmrb20;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Zepphod on 23/11/2015.
+ */
+
+@Mod(modid = "LMRB20", name = "LMRB 2.0", version ="1.7.10-0.01")
+public class LMRB20 {
+
+    @Mod.Instance("LMRB20")
+    public static LMRB20 instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void init (FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit (FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
